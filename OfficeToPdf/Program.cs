4fb93b3f8aeda9d6aaecebfe932b8db13bf3ea88
Francesco Furiani <francesco@clickmeter.com>
2016-08-31 12:52:44 +0200

    Code

---
+++ OfficeToPdf/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+using OfficeToPdf.Web;
+
+namespace OfficeToPdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri uri = new Uri("http://localhost:8000/converter");
+            var serviceImpl = new OfficeToPdf.Web.WebContract();
+            using (var host = new WebServiceHost(serviceImpl, uri))
+            {
+                host.Open();
+
+                Console.WriteLine("Service up and running at:");
+                foreach (var ea in host.Description.Endpoints)
+                {
+                    Console.WriteLine(ea.Address);
+                }
+
+                Console.ReadLine();
+                host.Close();
+            }
+        }
+    }
+}
---
+++ OfficeToPdf/Web/WebContract.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Office.Interop.Word;
+
+namespace OfficeToPdf.Web
+{
+    [ServiceContract]
+    [ServiceBehavior(
+        InstanceContextMode = InstanceContextMode.Single,
+        AddressFilterMode = AddressFilterMode.Any,
+        ConcurrencyMode = ConcurrencyMode.Single, IncludeExceptionDetailInFaults = true)]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class WebContract
+    {
+        [WebInvoke(Method = "POST", UriTemplate = "topdf", BodyStyle = WebMessageBodyStyle.Bare)]
+        public Stream ConvertFrom(Stream input)
+        {
+            MultipartParser parser = new MultipartParser(input);
+
+            if (parser.Success)
+            {
+                var randomGuid = System.Guid.NewGuid();
+                var tempFilename = randomGuid + "-" + parser.Filename;
+                using (
+                    var stream =
+                        new FileStream(Directory.GetCurrentDirectory() + @"\" + tempFilename, FileMode.Create, FileAccess.Write))
+                {
+                    stream.Write(parser.FileContents, 0, parser.FileContents.Length);
+                }
+
+                var appWord = new Microsoft.Office.Interop.Word.Application();
+                var wordDocument = appWord.Documents.Open(Directory.GetCurrentDirectory() + @"\" + tempFilename);
+                wordDocument.ExportAsFixedFormat(Directory.GetCurrentDirectory() + @"\" + tempFilename + ".pdf", WdExportFormat.wdExportFormatPDF);
+                appWord.Documents.Close();
+
+                var pdfBytes = File.ReadAllBytes(Directory.GetCurrentDirectory() + @"\" + tempFilename + ".pdf");
+
+                try
+                {
+                    File.Delete(Directory.GetCurrentDirectory() + @"\" + tempFilename);
+                    File.Delete(Directory.GetCurrentDirectory() + @"\" + tempFilename + ".pdf");
+                } catch (Exception)
+                { }
+
+                WebOperationContext.Current.OutgoingResponse.Headers["Content-Disposition"] = "attachment; filename=\"" + parser.Filename + ".pdf\"";
+                WebOperationContext.Current.OutgoingResponse.ContentType = "application/octet-stream";
+                return new MemoryStream(pdfBytes);
+            }
+            else
+            {
+                throw new WebException(System.Net.HttpStatusCode.UnsupportedMediaType.ToString());
+            }
+        }
+    }
+}
